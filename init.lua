cf429b91e4a82d4cb505723de94f6b870896386b
Ronak Chauhan <ronakchauhan97@gmail.com>
2026-07-07 22:41:52 +0000

    Comment cleanup

--- init.lua
+++ init.lua
@@ -99,9 +99,6 @@ vim.keymap.set("n", "<C-g>", "<cmd>Termdebug<CR>")
 -- NERDTree
 vim.keymap.set("n", "<C-t>", "<cmd>NERDTreeToggle<CR>")
 
--- " If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
--- autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
-
 
 -- airline
 -- vim.g["airline_powerline_fonts"] = 1 -- requires powerline fonts to be installed
